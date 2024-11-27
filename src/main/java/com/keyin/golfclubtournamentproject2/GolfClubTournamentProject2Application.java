972fe9b652005a40e929f361f13f88794d977255
nayajek <ifunanya.ejeckam@keyin.com>
2024-11-26 23:26:16 -0330

    Initial commit

---
+++ src/main/java/com/keyin/golfclubtournamentproject2/GolfClubTournamentProject2Application.java
@@ -0,0 +1,13 @@
+package com.keyin.golfclubtournamentproject2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GolfClubTournamentProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GolfClubTournamentProject2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/keyin/golfclubtournamentproject2/Tournament/Tournament.java
@@ -0,0 +1,4 @@
+package com.keyin.golfclubtournamentproject2.Tournament;
+
+public class Tournament {
+}
